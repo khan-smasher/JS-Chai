c425e57850c58bb4d41eef65ee5b15434dd66f7b
khan-smasher <167124942+khan-smasher@users.noreply.github.com>
2025-06-19 05:29:03 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+// let score = 33;
+// let score = "33";
+// let score = "33abc";
+// let score = null;
+// let score = undefined;
+// let score = true;
+// let score = false;
+// let score = [1, 2, 3];
+// let score = { name: "John" };
+// let score = function() { return "Hello"; };
+//let score = Symbol("unique"); // Symbol is not convertible to a number
+// let score = BigInt(12345678901234567890); // BIGINT is not convertible to a number
+// let score = new Date(); // Date is not directly convertible to a number
+// let score = /abc/; // Regular expressions are not directly convertible to a number
+
+
+
+console.log(typeof score);
+
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
