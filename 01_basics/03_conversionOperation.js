8ca0ad0c2d545f68d562d60bed7e9161a5d8a0a8
khan-smasher <167124942+khan-smasher@users.noreply.github.com>
2025-06-19 05:43:43 +0000

    coversions and some operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -13,13 +13,43 @@
 // let score = new Date(); // Date is not directly convertible to a number
 // let score = /abc/; // Regular expressions are not directly convertible to a number
 
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// ************ Operation ************
+let value = 3;
+let negValue = -value;
+console.log(negValue); // -3
+
+let str1 = "Hello";
+let str2 = "Khan";
+
+let str3 = str1 + " " + str2;
+console.log(str3); // Hello Khan
+
+console.log("1" + 2); // "12"
+console.log(1 + "2"); // "12"
+console.log("1" + true); // "1true"
+console.log(true + "1"); // "true1"
+// same goes with null ,undefined and NaN and other types
+// The + operator is overloaded in JavaScript for both addition and string concatenation.
+console.log("1" + "1"); // "11"
+console.log("1" + 2 + 2); // "122"
+console.log(1 + 2 + "2"); // "32"
+
+console.log(true); // true
+console.log(+true); // 1
+// console.log(true+); // SyntaxError: Unexpected token '+'
+
+console.log(+""); // 0
+
+// let n1 , n2, n3
+
+// n1 = n2 = n3 = 10;
+// console.log(n1, n2, n3); // 10 10 10
 
 
-console.log(typeof score);
-
-// console.log(typeof (score));
-
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
 
